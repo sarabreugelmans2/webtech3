02d0e4e4a2a009f13ccedf78c4608d5ad5c89b92
Sara Breugelmans <sara_breugelmans@hotmail.com>
2018-03-11 15:27:14 +0100

    html on localhost port 3000

--- week4/app.js
+++ week4/app.js
@@ -1,20 +1,18 @@
 
-//Load HTTP module
+//Load HTTP modules
 var http = require("http");
 var fs = require("fs");
 
-//Create HTTP server and listen on port 8000 for requests
 
+//nieuwe variable server maken
 var server= http.createServer(function (req, res) {
 
    console.log("request was made: "+ req.url);
-   res.writeHead(200, {'Content-Type': 'text/plain'});
+   res.writeHead(200, {'Content-Type': 'text/html'});
+   var myReadStream=fs.createReadStream(__dirname + '/wat-is-express-js.html', 'utf8');
+   myReadStream.pipe(res);
 
-   // Send the response body "Hello World"
-   res.end('Hello World\n');
+
 });
 server.listen(3000);
 console.log("server luistert naar poort 3000");
-
-// Print URL for accessing server
-console.log('Server running at http://127.0.0.1:8000/')

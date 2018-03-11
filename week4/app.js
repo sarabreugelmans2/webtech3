4725ff56d5c86e0fa2dde7e996fcb9397d7d8d75
Sara Breugelmans <sara_breugelmans@hotmail.com>
2018-03-11 17:26:11 +0100

    static html page through express

--- week4/app.js
+++ week4/app.js
@@ -1,27 +1,9 @@
+var express= require('express');
+var app= express();
 
-//Load HTTP modules
-var http = require("http");
-var fs = require("fs");
-
-
-//nieuwe variable server maken
-var server= http.createServer(function (req, res) {
-
-   console.log("request was made: "+ req.url);
-   if(req.url ==='/wat-is-express-js' ){
-   res.writeHead(200, {'Content-Type': 'text/html'});
-   //van waar wil je uitlezen, wat wil je lezen?
-   fs.createReadStream(__dirname + '/wat-is-express-js.html', 'utf8').pipe(res);}
-   //pipe om readstream te lezen en te schrijven in 1 keer
-   else{
-     res.writeHead(404, {'Content-Type': 'text/html'});
-     //van waar wil je uitlezen, wat wil je lezen?
-     fs.createReadStream(__dirname + '/404.html', 'utf8').pipe(res);
-   }
-
-
-
+app.get('/wat-is-express-js', function(req,res){
+  res.sendFile(__dirname + '/wat-is-express-js.html');
 });
-server.listen(3000);
-//welke poort
-console.log("server luistert naar poort 3000");
+
+app.listen(3000);
+console.log("U luistert naar poort 3000");
